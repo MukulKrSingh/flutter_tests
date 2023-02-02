3a7530cf318f602eaf90b4d1aa5a4d181e65d02b
mukul <singhmukul96@gmail.com>
2023-02-02 18:10:44 +0530

    1. added a tab view. 2. Integration test failing- as new  code added

--- lib/src/feature/home/presentation/home_screen.dart
+++ lib/src/feature/home/presentation/home_screen.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tests/src/widgets/app_bar_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  //late TabController _tabController;
+
+  @override
+  void initState() {
+    //_tabController = TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: getAppBar('Home'),
-      body: const Center(
-        child: Text(
-          'Home Screen',
-          key: Key('home_screen_text'),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          bottom: const TabBar(
+
+            tabs:  [
+              Icon(Icons.dashboard),
+              Icon(Icons.qr_code),
+              Icon(Icons.video_call),
+              Icon(Icons.chat_bubble),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.dashboard),
+            Icon(Icons.qr_code_2_outlined),
+            Icon(Icons.call),
+            Icon(Icons.chat_bubble),
+          ],
         ),
       ),
     );
   }
 }
+
+// flutter test integration_test/app_test.dart
